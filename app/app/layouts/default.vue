c9f1f6acf1397fc63a53d3165d3857e0a109370d
Edward-Mercado <edwardmercado149@gmail.com>
2026-07-20 09:54:42 -0400

    lets start settings

--- app/app/layouts/default.vue
+++ app/app/layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <div class="sticky h-[7vh] bg-[#FFDF88] flex justify-between items-center px-[1%]">
-            <h1 class="forum text-2xl hover:underline" @click="async () => {await navigateTo('/')}">Website Name</h1>
-            <div class="flex lg:w-[30%] w-[70%] justify-around">
+            <h1 class="forum text-2xl hover:underline" @click="async () => {await navigateTo('/')}">SITHS Book Review</h1>
+            <div class="flex lg:w-[40%] w-[70%] justify-around">
                 <button @click="async () => {await navigateTo('/catalog')}"
                     class="forum text-2xl bg-[#FAE6DA] border-2 border-[#ffcbab] px-[5%] rounded-2xl transition-all duration-300 ease-in-out hover:bg-[#ffdac9] hover:-translate-y-[2%] active:translate-y-[2%]">
                     Catalog </button>
---
+++ app/app/pages/settings.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-[#FEDBB3] h-screen p-[1%] flex flex-col gap-6">
+        
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
