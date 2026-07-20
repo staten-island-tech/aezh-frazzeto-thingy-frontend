b915a4c89039ade3c1b54cea0e7a181ba83857ff
Edward-Mercado <edwardmercado149@gmail.com>
2026-07-20 09:47:58 -0400

    index done

--- app/app/layouts/default.vue
+++ app/app/layouts/default.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
         <div class="sticky h-[7vh] bg-[#FFDF88] flex justify-between items-center px-[1%]">
-            <h1 class="forum text-2xl">Website Name</h1>
-            <div class="flex lg:w-[30%] w-[60%] justify-around">
-                <button
+            <h1 class="forum text-2xl hover:underline" @click="async () => {await navigateTo('/')}">Website Name</h1>
+            <div class="flex lg:w-[30%] w-[70%] justify-around">
+                <button @click="async () => {await navigateTo('/catalog')}"
                     class="forum text-2xl bg-[#FAE6DA] border-2 border-[#ffcbab] px-[5%] rounded-2xl transition-all duration-300 ease-in-out hover:bg-[#ffdac9] hover:-translate-y-[2%] active:translate-y-[2%]">
                     Catalog </button>
-                <button
+                <button @click="async () => {await navigateTo('/classes')}"
                     class="forum text-2xl bg-[#FAE6DA] border-2 border-[#ffcbab] px-[5%] rounded-2xl transition-all duration-300 ease-in-out hover:bg-[#ffdac9] hover:-translate-y-[2%] active:translate-y-[2%]">
                     Classes </button>
-                <h2 class="forum text-2xl">username</h2>
+                <h2 class="forum text-2xl hover:underline" @click="async () => {await navigateTo('/settings')}">
+                    username
+                </h2>
             </div>
         </div>
         <slot></slot>
@@ -17,7 +19,6 @@
 </template>
 
 <script setup lang="ts">
-
 </script>
 
 <style scoped></style>
--- app/app/pages/index.vue
+++ app/app/pages/index.vue
@@ -1,25 +1,30 @@
 <template>
-    <div class="bg-[#FEDBB3] h-screen p-[1%]">
-        <div class="w-full h-[50%] flex justify-between p-[0.5%]">
+    <div class="bg-[#FEDBB3] h-screen p-[1%] flex flex-col gap-6">
+        <div class="w-full h-[65%] flex justify-between p-[0.5%] gap-2">
             <div class="flex flex-col w-[50%] justify-center gap-2">
                 <h3 class="text-[3rem] forum"> Welcome! What will you be doing today? </h3>
                 <div class="flex flex-row bg-[#FFCCA7] h-[30%] rounded-2xl border-2 border-[#FF9D00] flex flex-row items-center justify-between px-[1%]">
                     <div class="flex flex-col p-[3%] justify-center">
-                        <h6 class="text-xl forum"> Trying to get to work? </h6>
-                        <p class="forum text-sm"> See your upcoming assignments. </p>
+                        <h6 class="md:text-xl forum text-lg"> Trying to get to work? </h6>
+                        <p class="forum md:text-sm text-xs"> See your upcoming assignments. </p>
                     </div>
-                    <button
-                    class="forum text-2xl bg-[#FAE6DA] border-2 border-[#ffcbab] px-[5%] rounded-2xl transition-all duration-300 ease-in-out hover:bg-[#ffdac9] hover:-translate-y-[2%] active:translate-y-[2%] h-[60%]">
-                    View Assignments </button>
+                    <button 
+                    class="forum text-2xl bg-[#FAE6DA] border-2 border-[#ffcbab] px-[5%] rounded-2xl transition-all duration-300 ease-in-out hover:bg-[#ffdac9] hover:-translate-y-[2%] active:translate-y-[2%] h-[60%]" @click="async () => {await navigateTo('/classes')}">
+                    View Classes </button>
                 </div>
             </div>
             <index-featured-catalog></index-featured-catalog>
+        </div>
+        <div class="flex flex-row bg-[#FFCCA7] h-[20%] rounded-2xl border-2 border-[#FF9D00] flex flex-col mt-[1%] p-[1%] justify-between">
+            <div v-for="opacity in ['100%', '80%', '60%', '40%', '20%']" :class="`bg-[#D9D9D9]/[${opacity}]`" class="w-full h-[16%] rounded-full">
+                
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-
+const acceptedClasses:string[] = [`bg-[#D9D9D9]/[100%]`, `bg-[#D9D9D9]/[80%]`, `bg-[#D9D9D9]/[60%]`, `bg-[#D9D9D9]/[40%]`, `bg-[#D9D9D9]/[20%]`]
 </script>
 
 <style scoped>
