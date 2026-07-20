8822a6eeacc8b3bc82d1a9bb4bd21d06f2cffd89
Andy-Zhang7223 <andyz127@nycstudents.net>
2026-07-20 11:04:23 -0400

    Attempting to apply divs and decorate them

---
+++ app/app/pages/catalog.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-[#FEDBB3] h-screen p-[1%]">
+        <div class="bg-[#FFCCA6] w-[95%] h-[90%] rounded-lg justify-center ">
+
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
