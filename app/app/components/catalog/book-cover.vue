3ea23526b1b3d67661feffddc4daede10b80a0e3
Andy-Zhang7223 <andyz127@nycstudents.net>
2026-07-21 11:48:35 -0400

    Catalog Page FrontEnd "Done"(I guess bro))

--- app/app/components/catalog/book-cover.vue
+++ app/app/components/catalog/book-cover.vue
@@ -2,17 +2,19 @@
   <div>
     <button @mouseenter="isHovered = true" @mouseleave="isHovered = false" class="relative">
       <img class="rounded-lg z-0" :src="book.cover" />
+      <transition class="transition-all duration-200 ease-in-out">
       <div
-        class="lg:bg-white absolute rounded-lg w-[40vw] h-[50vh] top-[0%] sm:bg-white w-[20vw] h-[20vw] z-50 flex flex-row transition-all duration-300 ease-in-out"
-        :class="focusedClasses"
+        class="bg-white absolute rounded-lg top-[0%] z-50 flex flex-row overflow-hidden hover:scale-110 origin-top-left"
+        :class="hoverClasses"
         v-if="isHovered">
-        <img class="rounded-lg aboslute w-[40%] min-h-[40%]" :src="book.cover" />
-        <div class="flex flex-col items-center justify-center w-full">
+        <img class="rounded-lg max-w-[18vw] aboslute" :src="book.cover" />
+        <div class="flex flex-col items-center justify-center w-[20vw] h-[20vw]">
           <h2 class="forum text-2xl w-full h-[40%] text-center">{{ book.name }}</h2>
           <h3 class="forum text-1x1 h-[30%] text-center">{{ book.author }}</h3>
           <h3 class="forum text-1x1 h-[30%] text-center">{{ book.rating }}/5</h3>
         </div>
       </div>
+      </transition>
     </button>
   </div>
 </template>
@@ -22,8 +24,8 @@ import { ref } from 'vue';
 
 const isHovered = ref<boolean>(false)
 
-const transitionTrigger = computed(() => {
-  return 
+const hoverClasses = computed(() => {
+  return isHovered.value ? ["max-w-[50vw]"]: ["max-h-[0vw]"]
 })
 
 defineProps({
